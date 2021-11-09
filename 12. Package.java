67d3de988c535527274b679df5ec9e7cba0e4019
choijiwoong <gogogi313@naver.com>
2021-11-09 22:38:49 +0900

    chap.12 Simple Concept of Package&SubPackage

---
+++ 12. Package.java
@@ -0,0 +1,30 @@
+//[1. Package]
+
+//HouseKim.java
+package Java.house;//Java(directory)\house(new folder will be made)
+public class HouseKim{}
+
+//HousePark.java
+package Java.house;//Java(directory)\house(new folder will be made)
+public class HousePark{}
+
+//[2. SubPackage]
+
+//EungYoungPark.java
+package Java.house.person;
+public class EungYongPark{}
+
+//structure of directory!
+//src/Java/house/HouseKim.java
+//src/Java/house/HousePark.java
+//src/Java/house/person/EungYongPark.java
+
+//bin/Java/house/HouseKim.class
+//bin/Java/house/HousePark.class
+//bin/Java/house/person/EungYongPark.class
+
+//[3. Use of Packages]
+//for using HouseKim class,
+import Java.house.HouseKim;
+//or
+import Java.house.*;
